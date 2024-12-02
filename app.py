daa4afc681e7234b5359fecc3ae55408be6fed37
ottawa-ehospital <103956379+ottawa-ehospital@users.noreply.github.com>
2024-12-03 00:42:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -63,4 +63,5 @@ def get_patient_data():
     })
  
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 for local testing
+    app.run(host="0.0.0.0", port=port)
